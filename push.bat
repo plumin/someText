7433303149f4b642ff9cd4351c833d128805a7f2
plumin <hellowoko@gmail.com>
2022-07-23 04:09:29 +0800

    04:09:27.07

--- push.bat
+++ push.bat
@@ -1,6 +1,5 @@
 @echo off
-java E:\Code_ipch\VS_ipch\e5_renew\ReadWeb
 set time0=%time: =0%
 cd %~dp0
-git add . && git commit -m %time0% && git push
+java ReadWeb && git add . && git commit -m %time0% && git push
 pause
